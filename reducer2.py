d8ae05399fcf0c9031ed0e839a0b428d5d1c0293
Letícia Montenegro <leticia.braga.montenegro@hotmail.com>
2023-08-17 21:09:05 -0400

    Update letter only regex reducer2.py

--- reducer2.py
+++ reducer2.py
@@ -13,7 +13,8 @@ result = {'palavras_distintas': None,
           'top_10': None,
           'histograma': None}
 
-letter_only_regex = re.compile(r"[A-Za-z]+")
+punctuation_regex = re.compile(r"[,.?!;:()\[\]{}-]")
+letter_only_regex = re.compile(r"^[A-Za-z]+$")
 counting = dict()
 
 for word in sys.stdin:
@@ -21,12 +22,13 @@ for word in sys.stdin:
 
     word, count = word.split('\t', 1)
     word = re.sub(
-        letter_only_regex, '', word
+        punctuation_regex, '', word
         ).replace("'", '').replace('"', '')
     try:
         quantity = int(count)
         if word in counting:
-            counting[word] += quantity
+            if letter_only_regex.match(word):
+                counting[word] += quantity
         else:
             counting[word] = quantity
     except ValueError:
@@ -36,5 +38,6 @@ ordered_counting = sorted(counting.items(), key=lambda x: x[1], reverse=True)
 result['palavras_distintas'] = len(ordered_counting)
 result['top_10'] = list(ordered_counting)[:10]
 histograma = [(word, count) for word, count in counting.items()]
+sorted_histograma = sorted(histograma, key=lambda x: x[1], reverse=True)
 result['histograma'] = histograma
 print(result)
